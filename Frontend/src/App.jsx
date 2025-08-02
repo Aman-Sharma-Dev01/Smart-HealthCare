108abf93f95516126e181e764cd1f40f88dcd588
Aman Sharma <asharma8464@gmail.com>
2025-08-02 20:47:47 +0530

    route fix

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as  Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HospitalDashboard from './Components/HospitalDashboard/HospitalDashboard';
 import HomePage from './Components/HomePage/HomePage';
 
